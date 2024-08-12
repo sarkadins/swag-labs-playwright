56710b33450a1447ceabcd5f2240c7fa7230464e
sarkadins <soma.sarkadi@gmail.com>
2024-08-12 18:40:58 +0200

    - add ListProductsTest.java class - modify HomePage.java class

--- src/main/java/com/codecool/pages/HomePage.java
+++ src/main/java/com/codecool/pages/HomePage.java
@@ -8,18 +8,42 @@ public class HomePage extends BasePage {
     private final Locator productsTextLocator;
     private final Locator menuBtnLocator;
     private final Locator logoutBtnLocator;
+    private final Locator dropdownLocator;
 
     public HomePage(Page page) {
         super(page);
         this.productsTextLocator = page.locator(".title");
         this.menuBtnLocator = page.locator("#react-burger-menu-btn");
         this.logoutBtnLocator = page.locator("#logout_sidebar_link");
+        this.dropdownLocator = page.locator(".product_sort_container");
+
     }
-    public String getProductsText(){
+
+    public String getProductsText() {
         return productsTextLocator.textContent();
     }
-    public void logout(){
+
+    public void logout() {
         menuBtnLocator.click();
         logoutBtnLocator.click();
     }
+
+    public void listProductsByNameAToZ() {
+        dropdownLocator.click();
+        dropdownLocator.selectOption("az");
+    }
+
+    public void listProductsByNameZToA() {
+        dropdownLocator.click();
+        dropdownLocator.selectOption("za");
+    }
+    public void listProductsByPriceLowToHigh(){
+        dropdownLocator.click();
+        dropdownLocator.selectOption("lohi");
+    }
+    public void listProductsByPriceHighToLow(){
+        dropdownLocator.click();
+        dropdownLocator.selectOption("hilo");
+    }
+
 }
